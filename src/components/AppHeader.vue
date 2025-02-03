e81e628834f4739ececa808e9064147fbf5ef19f
Vladimir <pestov.web@yandex.ru>
2025-02-03 14:07:48 +0300

    chore(AppHeader): adding nav menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,20 @@
-<script></script>
+<script>
+import NavMenu from './NavMenu.vue';
 
-<template><header class="header"></header></template>
+export default {
+  name: 'AppHeader',
+  components: {
+    NavMenu,
+  },
+};
+</script>
+
+<template>
+  <header class="header">
+    <div class="header__container">
+      <NavMenu />
+    </div>
+  </header>
+</template>
 
 <style lang="scss"></style>
